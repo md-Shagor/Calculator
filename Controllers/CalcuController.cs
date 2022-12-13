605d3b3dcaf21b881a6c5f0e80258d783cb7b510
Shagor <105841627+md-Shagor@users.noreply.github.com>
2022-12-13 18:50:40 +0600

    Add files via upload

---
+++ Controllers/CalcuController.cs
@@ -0,0 +1,46 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcuController : Controller
+    {
+        // GET: Calu
+        public ActionResult Index()
+        {
+            return View(new Calcu());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(Calcu c, string Calculate)
+        {
+            if (Calculate == "add")
+            {
+                c.Op = c.Num1 + c.Num2;
+            }
+            else if (Calculate == "min")
+            {
+                c.Op = c.Num1 - c.Num2;
+            }
+            else if (Calculate == "sub")
+            {
+                c.Op = c.Num1 * c.Num2;
+            }
+            else
+            {
+                c.Op = c.Num1 / c.Num2;
+            }
+            return View(c);
+        }
+        public ActionResult Mycal()
+        { 
+            return View();
+        }
+
+    }
+}
---
+++ Models/Calcu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class Calcu
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Op { get; set; }
+    }
+}
